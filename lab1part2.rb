80222c7bf400e787d14f68a4b4d76082b2201d9e
Malcolm314159 <mhlittlefield@gmail.com>
2016-04-26 19:56:38 -0400

    test123
    I put my .rb files in my cloned git repository

---
+++ lab1part2.rb
@@ -0,0 +1,9 @@
+puts "The method name_made_lib() is definted!"
+
+def name_mad_lib()
+	puts "What is your name?"
+	name = gets.to_s
+	puts "How old are you?"
+	age = gets.to_s
+	puts "Your name is "+name+" and you are "+age+" years old."
+end
---
+++ ruby_homework_1.rb
@@ -0,0 +1,13 @@
+#Malcolm Littlefield
+#First Ruby Program!
+#April 2016
+
+puts "Running Malcolm's first ruby homework!"
+puts " "
+puts "the function celtofar() is defined.  Call it!"
+
+def celtofar()
+	a = gets.to_i
+	b = a*9/5+32
+	return b
+end
