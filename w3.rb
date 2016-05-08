1b2ff0d79345971dc807c6093c9b2f4bb55f213e
Malcolm314159 <mhlittlefield@gmail.com>
2016-05-08 11:33:58 -0400

    Added files via upload

--- w3.rb
+++ w3.rb
@@ -6,22 +6,31 @@ chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 chars += "0123456789"
 chars += "!@#$%^&*()_+-=[]|{}:;'?/><,."
 
-cipher = Hash.new
-
+randomizer = Array.new #this will be an array with one integer for each character
 for i in 0..(chars.length-1)
-	cipher[chars[i]] = chars[rand(chars.length)]
+	randomizer << i
+end
+randomizer = randomizer.shuffle
+
+$cipher = Hash.new #A hash of all characters. values are shuffled around
+a = 0
+for i in randomizer
+	$cipher[chars[a]] = chars[i]
+	a += 1
 end
 
 def encrypt(a)
 	b = String.new
 	for i in 0..(a.length-1)
-		b += cipher[a[i]]
+		b += $cipher[a[i]]
 	end
+	return b
 end
 
 def decrypt(a)
 	b = String.new
 	for i in 0..(a.length-1)
-		b += cipher.key(a[i])
+		b += $cipher.key(a[i])
 	end
+	return b
 end
